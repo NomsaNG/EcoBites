8034c6460dcad25e238ff87bc208b23af698028b
Nomsa Goba <nomsanatashagoba@gmail.com>
2023-11-22 16:47:29 +0200

    dropdown and dynamic coding

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :orders
-  has_many :offers
+  has_many :orders, dependent: :destroy
+  has_many :offers, dependent: :destroy
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
