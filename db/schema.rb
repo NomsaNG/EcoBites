e3417ead8260bbaa65553aaa320fbf9817666566
Kieran Huybrechts <kieranhuybrechts@gmail.com>
2023-11-23 11:58:05 +0200

    mostly just added 10 seeds and added hotdeals field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_22_102655) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_23_091021) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,6 +54,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_22_102655) do
     t.time "pickup_time_end"
     t.string "title"
     t.string "shop"
+    t.boolean "hotdeals"
     t.index ["user_id"], name: "index_offers_on_user_id"
   end
 
